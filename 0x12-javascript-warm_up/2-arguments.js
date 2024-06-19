f1b15ab112af789a0bdbfbaf2ac11a21a382d1b2
NezarMo7 <neln7aly11055@gmail.com>
2024-06-20 02:11:31 +0300

    2test

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 // check and print args
 
-if (process.argv.lenth === 2) {
-    console.log('No argument')
-} else if (process.argv.lenth === 3){
-    console.log('Argument found')
-} else {
-    console.log('Argument found')
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
