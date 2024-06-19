cc463258b1e64691a32d56de77d6c6af9cf79b88
NezarMo7 <neln7aly11055@gmail.com>
2024-06-20 01:58:47 +0300

    2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// check and print args
+
+if (process.argv.lenth === 2) {
+    console.log('No argument')
+} else if (process.argv.lenth === 3){
+    console.log('Argument found')
+} else {
+    console.log('Argument found')
+}
